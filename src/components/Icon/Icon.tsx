d7da239631e976ed095e4b2665c8f0eed3c89123
Miha Arh <miha.arh06@gmail.com>
2022-09-06 22:58:48 +0200

    Added basic Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import React from 'react';
+
+import styles from './Icon.module.css';
+
+interface IconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  size: 'small' | 'large';
+  alt: string;
+  className?: string;
+}
+function Icon({ size, alt, className, ...props }: IconProps) {
+  return (
+    <img
+      className={classNames(size === 'small' ? styles.smallIcon : styles.largeIcon, className)}
+      alt={alt}
+      {...props}
+    />
+  );
+}
+
+export default Icon;
