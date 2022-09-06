e610f033b0f4f05a5f36ed87ab7afc1bf3d2430d
Miha Arh <miha.arh06@gmail.com>
2022-09-06 18:43:27 +0200

    Implemented SplashScreen component

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,7 +1,19 @@
+import Logo from 'assets/images/Logo.png';
+import classNames from 'classnames';
 import React from 'react';
 
-function SplashScreen() {
-  return <div>SplashScreen</div>;
+import styles from './SplashScreen.module.css';
+
+interface SplashScreenProps {
+  onComplete: () => void;
+}
+
+function SplashScreen({ onComplete }: SplashScreenProps) {
+  return (
+    <div className={classNames(styles.container, styles.appear)} onAnimationEnd={onComplete}>
+      <img src={Logo} alt="MordOS Logo" className={styles.img} />
+    </div>
+  );
 }
 
 export default SplashScreen;
