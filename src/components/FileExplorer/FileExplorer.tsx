c3258d21d74f9be61876e0bc70f4dc4c54608984
Miha Arh <miha.arh06@gmail.com>
2022-09-10 01:03:25 +0200

    Implemented delete file logic

--- src/components/FileExplorer/FileExplorer.tsx
+++ src/components/FileExplorer/FileExplorer.tsx
@@ -1,46 +1,74 @@
+import classNames from 'classnames';
 import AppWindow from 'components/AppWindow';
 import FlexDiv from 'components/FlexDiv';
 import Icon from 'components/Icon';
+import useAppDispatch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
 import { Icons } from 'models/constants';
-import React from 'react';
-import { selectDirectoryName, selectDirectoryPath, selectFiles } from 'store/directorySlice';
+import React, { useState } from 'react';
+import { deleteFile, selectDirectoryName, selectDirectoryPath, selectFiles } from 'store/directorySlice';
 
 import styles from './FileExplorer.module.css';
 
 function FileExplorer() {
+  const [selectedFile, setSelectedFile] = useState<number | null>(null);
+  const [areLeftIconsDisabled, setAreLeftIconsDisabled] = useState(true);
   const files = useAppSelector(selectFiles);
   const directoryName = useAppSelector(selectDirectoryName);
   const directoryPath = useAppSelector(selectDirectoryPath);
-
+  const dispatch = useAppDispatch();
   const rightFooter = `${files.length} ${files.length === 1 ? 'file' : 'files'}`;
-
+  function deleteItem() {
+    if (selectedFile !== null) {
+      dispatch(deleteFile(selectedFile));
+    }
+  }
   function renderItems() {
-    return files.map(item => (
-      <FlexDiv key={`${item.name}${item.createdAt}`} className={styles.row}>
-        <div className={styles.rowIcon}>
-          <Icon src={Icons.FILE_SOLID} alt="File" size="small" />
-        </div>
-        <div className={styles.rowItem}>{item.name}</div>
-        <div className={styles.rowItem}>{item.updatedAt}</div>
-      </FlexDiv>
-    ));
+    return files.map(item => {
+      function selectItem() {
+        if (item.id === selectedFile) {
+          setSelectedFile(null);
+          setAreLeftIconsDisabled(true);
+          return;
+        }
+        setAreLeftIconsDisabled(false);
+        setSelectedFile(item.id);
+      }
+      function openItem() {}
+      return (
+        <FlexDiv
+          key={item.id}
+          className={classNames(styles.row, item.id === selectedFile ? styles.selected : '')}
+          onClick={selectItem}
+          onDoubleClick={openItem}>
+          <div className={styles.rowIcon}>
+            <Icon src={Icons.FILE_SOLID} alt="File" size="small" />
+          </div>
+          <div className={styles.rowItem}>{item.name}</div>
+          <div className={styles.rowItem}>{item.updatedAt}</div>
+          <div className={styles.rowItem}>{item.createdAt}</div>
+        </FlexDiv>
+      );
+    });
   }
 
   return (
     <AppWindow
       isResizable
       title={directoryName}
+      leftIcons={
+        <Icon src={Icons.TRASH} size="small" alt="Delete" disabled={areLeftIconsDisabled} onClick={deleteItem} />
+      }
       footerLeft={<FlexDiv>{directoryPath}</FlexDiv>}
       footerRight={rightFooter}>
       <FlexDiv className={styles.table}>
         <FlexDiv className={styles.header}>
           <div className={styles.rowItem}>Name</div>
           <div className={styles.rowItem}>Last Modified</div>
-          <div className={styles.rowItem}>Kind</div>
+          <div className={styles.rowItem}>Created</div>
         </FlexDiv>
         <div className="divider" />
-        <FlexDiv className={styles.body}>{!files ? 'Directory is Empty' : renderItems()}</FlexDiv>
+        <FlexDiv className={styles.body}>{renderItems()}</FlexDiv>
       </FlexDiv>
     </AppWindow>
   );
--- src/store/directorySlice.ts
+++ src/store/directorySlice.ts
@@ -3,10 +3,13 @@ import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from './configureStore';
 
 export interface File {
+  id: number;
   name: string;
   createdAt: number;
   updatedAt: number;
   content: string;
+  selected: boolean;
+  oppened: boolean;
 }
 export interface DirectoryState {
   name: string;
@@ -19,10 +22,13 @@ const initialState: DirectoryState = {
   path: 'MordOs\\Files',
   files: [
     {
+      id: 0,
       name: 'README.md',
       createdAt: 0,
       updatedAt: 0,
       content: '# MordOs',
+      selected: false,
+      oppened: false,
     },
   ],
 };
@@ -31,16 +37,26 @@ export const directorySlice = createSlice({
   name: 'directory',
   initialState,
   reducers: {
+    deleteFile: (state, action: PayloadAction<number>) => {
+      state.files = state.files.filter(file => file.id !== action.payload);
+    },
     addFile: (state, action: PayloadAction<File>) => {
       state.files.push(action.payload);
     },
     addFileToDirectory: (state, action: PayloadAction<{ file: File; path: string[] }>) => {
       const { file, path } = action.payload;
     },
+    selectItem: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const selectedFile = state.files.find(file => file.id === id);
+      if (selectedFile) {
+        selectedFile.selected = !selectedFile.selected;
+      }
+    },
   },
 });
 
-export const { addFile } = directorySlice.actions;
+export const { addFile, deleteFile } = directorySlice.actions;
 
 export const selectRootDirectory = (state: RootState) => state.directory;
 export const selectDirectoryName = createSelector(selectRootDirectory, directory => directory.name);
