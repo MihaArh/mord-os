2d6fbe037f306141abe5acd56c634ad631443ac3
Miha Arh <miha.arh06@gmail.com>
2022-09-09 15:31:16 +0200

    Added icons and tooltip

--- src/components/Dock/Dock.tsx
+++ src/components/Dock/Dock.tsx
@@ -1,7 +1,87 @@
+import FlexDiv from 'components/FlexDiv';
+import Icon from 'components/Icon';
+import { Icons } from 'models/constants';
 import React from 'react';
 
+import styles from './Dock.module.css';
+
+const APPS = [
+  {
+    name: 'Notes',
+    icon: Icons.NOTES,
+    tooltip: 'New Note',
+    component: '',
+    onClick: () => {
+      console.log('Notes');
+    },
+  },
+  {
+    name: 'Files',
+    icon: Icons.FOLDER,
+    tooltip: 'Files',
+    component: '',
+    onClick: () => {
+      console.log('Files');
+    },
+  },
+  {
+    name: 'Browser',
+    icon: Icons.BROWSER,
+    tooltip: 'Browser',
+    component: '',
+    onClick: () => {
+      console.log('Browser');
+    },
+  },
+  {
+    name: 'Camera',
+    icon: Icons.CAMERA,
+    tooltip: 'Camera',
+    component: '',
+    onClick: () => {
+      console.log('Camera');
+    },
+  },
+  {
+    name: 'Gallery',
+    icon: Icons.IMAGE,
+    tooltip: 'Gallery',
+    component: '',
+    onClick: () => {
+      console.log('Gallery');
+    },
+  },
+  {
+    name: 'News',
+    icon: Icons.NEWS,
+    tooltip: 'News',
+    component: '',
+    onClick: () => {
+      console.log('News');
+    },
+  },
+];
 function Dock() {
-  return <div>Dock</div>;
+  function renderDockIcons() {
+    return APPS.map(app => (
+      <FlexDiv className={styles.iconContainer} key={app.name}>
+        <div className={styles.tooltip}>{app.tooltip}</div>
+        <Icon
+          size="large"
+          src={app.icon}
+          alt={app.name}
+          onClick={app.onClick}
+          className={styles.icon}
+          data-text={app.tooltip}
+        />
+      </FlexDiv>
+    ));
+  }
+  return (
+    <FlexDiv className={styles.container}>
+      <FlexDiv className={styles.iconsContainer}>{renderDockIcons()}</FlexDiv>
+    </FlexDiv>
+  );
 }
 
 export default Dock;
--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,8 +1,14 @@
 import arrowRight from 'assets/icons/arrow-right.svg';
+import browser from 'assets/icons/browser.svg';
+import camera from 'assets/icons/camera.svg';
 import eyeDisable from 'assets/icons/eye-disable.svg';
 import eyeIcon from 'assets/icons/eye.svg';
+import folder from 'assets/icons/folder.svg';
+import image from 'assets/icons/image.svg';
 import lockIcon from 'assets/icons/lock.svg';
 import logo from 'assets/icons/logo.svg';
+import news from 'assets/icons/news.svg';
+import notes from 'assets/icons/notes.svg';
 import tick from 'assets/icons/tick.svg';
 import userIcon from 'assets/icons/user.svg';
 import avatar from 'assets/images/Avatar.png';
@@ -15,6 +21,12 @@ export const Icons = {
   ARROW_RIGHT: arrowRight,
   TICK: tick,
   LOGO: logo,
+  CAMERA: camera,
+  FOLDER: folder,
+  BROWSER: browser,
+  IMAGE: image,
+  NEWS: news,
+  NOTES: notes,
 };
 
 export const Images = {
