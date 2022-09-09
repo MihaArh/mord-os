e314ae433134ba97eacb258974e8a57dcea33418
Miha Arh <miha.arh06@gmail.com>
2022-09-09 19:13:40 +0200

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import SplashScreen from 'components/SplashScreen';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Authentication from 'screens/Authentication';
+import Desktop from 'screens/Desktop';
 
 import './App.css';
 
 function App() {
-  return <div />;
+  const [isSplashScreenComplete, setIsSplashScreenComplete] = useState(false);
+  function onSplashScreenComplete() {
+    setIsSplashScreenComplete(true);
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SplashScreen onComplete={onSplashScreenComplete} />} />
+        <Route path="/login" element={<Authentication />} />
+        <Route path="/desktop" element={<Desktop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
