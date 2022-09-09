d20cb204600a4c14dbef6037436e471f2bdaacc1
Miha Arh <miha.arh06@gmail.com>
2022-09-10 01:03:35 +0200

    Added disabled property

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,16 +6,18 @@ import styles from './Icon.module.css';
 interface IconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   size: 'small' | 'large';
   alt: string;
+  disabled?: boolean;
   onClick?: () => void;
   className?: string;
 }
-function Icon({ size: iconSize, alt, className, onClick, ...props }: IconProps) {
+function Icon({ size: iconSize, alt, disabled = false, className, onClick, ...props }: IconProps) {
   return (
-    <button onClick={onClick} type="button" className={styles.button}>
+    <button onClick={onClick} type="button" className={styles.button} disabled={disabled}>
       <img
         className={classNames(
           iconSize === 'small' ? styles.smallIcon : styles.largeIcon,
           onClick ? styles.clickable : '',
+          disabled ? styles.disabled : '',
           className,
         )}
         alt={alt}
