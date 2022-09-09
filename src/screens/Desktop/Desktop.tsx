7e07967bd200ad6a9b93ebcdc57681e6803fe0ab
Miha Arh <miha.arh06@gmail.com>
2022-09-09 19:13:29 +0200

    Changed to FileEditor component

--- src/screens/Desktop/Desktop.tsx
+++ src/screens/Desktop/Desktop.tsx
@@ -1,6 +1,6 @@
-import AppWindow from 'components/AppWindow';
 import DesktopBackground from 'components/DesktopBackground';
 import Dock from 'components/Dock';
+import FileEditor from 'components/FileEditor';
 import FlexDiv from 'components/FlexDiv';
 import MenuBar from 'components/MenuBar';
 import React from 'react';
@@ -15,9 +15,7 @@ function Desktop() {
           <MenuBar />
         </FlexDiv>
         <FlexDiv className={styles.contentContainer}>
-          <AppWindow title="App" isResizable>
-            <div>Test</div>
-          </AppWindow>
+          <FileEditor />
         </FlexDiv>
         <FlexDiv className={styles.dockContainer}>
           <Dock />
