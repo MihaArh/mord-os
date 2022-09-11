a521f159b2145dacb1edb5df7fa8567b524fcb8c
Miha Arh <miha.arh06@gmail.com>
2022-09-11 20:16:18 +0200

    Removed user credentials

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -54,8 +54,3 @@ export const Images = {
 export const EMAIL_REGEX =
   /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z\s]{2,}))$/;
 export const PASSWORD_REGEX = /^.{8,}$/;
-
-export const USER = {
-  EMAIL: 'borgoth@mordos.com',
-  PASSWORD: '12bindthem',
-};
