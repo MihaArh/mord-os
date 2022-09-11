57636a16dab634fabbfd2d43bcefcf2df6944908
Miha Arh <miha.arh06@gmail.com>
2022-09-11 20:50:15 +0200

    Added guard to only allow logged in user to view desktop

--- src/screens/Desktop/Desktop.tsx
+++ src/screens/Desktop/Desktop.tsx
@@ -5,13 +5,22 @@ import FileExplorer from 'components/FileExplorer';
 import FlexDiv from 'components/FlexDiv';
 import MenuBar from 'components/MenuBar';
 import useAppSelector from 'hooks/useAppSelector';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { selectOpenedApps } from 'store/applicationsSlice';
+import { selectLoggedIn } from 'store/userSlice';
 
 import styles from './Desktop.module.css';
 
 function Desktop() {
   const openedApps = useAppSelector(selectOpenedApps);
+  const isLoggedIn = useAppSelector(selectLoggedIn);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/');
+    }
+  });
   return (
     <DesktopBackground>
       <FlexDiv className={styles.container}>
